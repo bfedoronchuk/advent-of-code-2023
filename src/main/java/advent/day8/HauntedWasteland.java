afa289989e282278d197b0c69032e684ee28ea76
bfedoronchuk <bfedodoronchuk@gmail.com>
2023-12-09 18:35:59 +0200

    Day 8: Haunted Wasteland (Part 2)

--- src/main/java/advent/day8/HauntedWasteland.java
+++ src/main/java/advent/day8/HauntedWasteland.java
@@ -24,16 +24,7 @@ public class HauntedWasteland {
             try (BufferedReader reader = ResourceUtils.resourceReader(HauntedWasteland.class, MAP)) {
                 List<String> lines = reader.lines().toList();
                 List<Direction> directions = readDirections(lines.get(0));
-                Map<String, SimpleEntry<String, String>> navigations = lines.stream()
-                        .skip(2)
-                        .collect(Collector.of(
-                                HashMap::new,
-                                Solution1::addNavigation,
-                                (map1, map2) -> {
-                                    map1.putAll(map2);
-                                    return map1;
-                                }
-                        ));
+                Map<String, SimpleEntry<String, String>> navigations = readNavigations(lines);
                 int result = countSteps(directions, navigations);
                 logger.log(Level.INFO, "Result = {0}", result);
             } catch (IOException ex) {
@@ -59,31 +50,100 @@ public class HauntedWasteland {
 
             return steps;
         }
+    }
 
-        private static void addNavigation(HashMap<String, SimpleEntry<String, String>> map, String line) {
-            Objects.requireNonNull(map);
-            Objects.requireNonNull(line);
-            String[] tokens = line.split(" = ");
-            String node = tokens[0];
-            String[] leftAndRightTokens = tokens[1]
-                    .replaceAll("[)(]", "")
-                    .split(", ");
-            String left = leftAndRightTokens[0];
-            String right = leftAndRightTokens[1];
-            map.put(node, new SimpleEntry<>(left, right));
+    public static class Solution2 {
+        public static void main(String[] args) {
+            try (BufferedReader reader = ResourceUtils.resourceReader(HauntedWasteland.class, MAP)) {
+                List<String> lines = reader.lines().toList();
+                List<Direction> directions = readDirections(lines.get(0));
+                Map<String, SimpleEntry<String, String>> navigations = readNavigations(lines);
+                long result = countSteps(directions, navigations);
+                logger.log(Level.INFO, "Result = {0}", result);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "Unable to read input file", ex);
+            }
         }
 
-        private static List<Direction> readDirections(String directionsLine) {
-            Objects.requireNonNull(directionsLine);
-            return Arrays.stream(directionsLine.split(""))
-                    .map(token -> {
-                        if ("L".equals(token)) {
-                            return Direction.LEFT;
-                        } else {
-                            return Direction.RIGHT;
+        private static long countSteps(List<Direction> directions,
+                                      Map<String, SimpleEntry<String, String>> navigations) {
+            List<String> startingNodes = navigations.keySet().stream()
+                    .filter(node -> node.endsWith("A"))
+                    .collect(Collectors.toList());
+
+            List<Long> stepsPerNode = startingNodes.stream()
+                    .map(node -> countStepsForNode(node, directions, navigations))
+                    .collect(Collectors.toList());
+            return leastCommonMultiple(stepsPerNode);
+        }
+
+        private static long leastCommonMultiple(List<Long> stepsPerNode) {
+            long lsm = 1L;
+            for (long steps: stepsPerNode) {
+                lsm = leastCommonMultiple(lsm, steps);
+            }
+            return lsm;
+        }
+
+        private static long leastCommonMultiple(long a, long b) {
+            return a * b / greatestCommonDivisor(a, b);
+        }
+
+        private static long greatestCommonDivisor(long a, long b) {
+            if (b == 0) {
+                return a;
+            }
+            return greatestCommonDivisor(b, a % b);
+        }
+
+        private static long countStepsForNode(String node,
+                                                 List<Direction> directions,
+                                                 Map<String, SimpleEntry<String, String>> navigations) {
+            long steps = 0L;
+            DirectionsInfiniteIterator directionsIterator = new DirectionsInfiniteIterator(directions);
+            String currentNode = node;
+            while (!currentNode.endsWith("Z")) {
+                Direction direction = directionsIterator.next();
+                SimpleEntry<String, String> nextSteps = navigations.get(currentNode);
+                currentNode = direction == Direction.LEFT ? nextSteps.getKey() : nextSteps.getValue();
+                steps++;
+            }
+            return steps;
+        }
+    }
+
+    private static HashMap<String, SimpleEntry<String, String>> readNavigations(List<String> lines) {
+        return lines.stream()
+                .skip(2)
+                .collect(Collector.of(
+                        HashMap::new,
+                        (map, line) -> {
+                            String[] tokens = line.split(" = ");
+                            String node = tokens[0];
+                            String[] leftAndRightTokens = tokens[1]
+                                    .replaceAll("[)(]", "")
+                                    .split(", ");
+                            String left = leftAndRightTokens[0];
+                            String right = leftAndRightTokens[1];
+                            map.put(node, new SimpleEntry<>(left, right));
+                        },
+                        (map1, map2) -> {
+                            map1.putAll(map2);
+                            return map1;
                         }
-                    }).collect(Collectors.toList());
-        }
+                ));
+    }
+
+    private static List<Direction> readDirections(String directionsLine) {
+        Objects.requireNonNull(directionsLine);
+        return Arrays.stream(directionsLine.split(""))
+                .map(token -> {
+                    if ("L".equals(token)) {
+                        return Direction.LEFT;
+                    } else {
+                        return Direction.RIGHT;
+                    }
+                }).collect(Collectors.toList());
     }
 
     public enum Direction {
