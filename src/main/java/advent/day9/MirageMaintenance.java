7d2930182ff4c3e1584b60858819844510eb31f0
bfedoronchuk <bfedodoronchuk@gmail.com>
2023-12-10 17:09:21 +0200

    Day 9: Mirage Maintenance (Part 2)

--- src/main/java/advent/day9/MirageMaintenance.java
+++ src/main/java/advent/day9/MirageMaintenance.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -19,7 +20,7 @@ public class MirageMaintenance {
             try (BufferedReader reader = ResourceUtils.resourceReader(MirageMaintenance.class, REPORT)) {
                 int result = reader.lines()
                         .map(MirageMaintenance::lineToValueHistory)
-                        .map(Solution1::extrapolateValueHistory)
+                        .map(history -> extrapolateValueHistory(history, Solution1::extrapolate))
                         .mapToInt(history -> history.get(history.size() - 1))
                         .sum();
                 logger.log(Level.INFO, "Result = {0}", result);
@@ -28,32 +29,62 @@ public class MirageMaintenance {
             }
         }
 
-        private static List<Integer> extrapolateValueHistory(List<Integer> history) {
-            List<List<Integer>> extrapolationResults = new ArrayList<>();
-            extrapolationResults.add(history);
-
-            List<Integer> currentSequence = history;
-
-            while (!currentSequence.stream().allMatch(value -> value == 0)) {
-                List<Integer> nextSequence = new ArrayList<>();
-                for (int i = 0; i < currentSequence.size() - 1; i++) {
-                    int difference =  currentSequence.get(i + 1) - currentSequence.get(i);
-                    nextSequence.add(difference);
-                }
-                if (!nextSequence.isEmpty()) {
-                    extrapolationResults.add(nextSequence);
-                }
-                currentSequence = nextSequence;
-            }
-
-            for (int i = extrapolationResults.size() - 1; i > 0; i--) {
-                List<Integer> sequence = extrapolationResults.get(i);
-                List<Integer> upperSequence = extrapolationResults.get(i - 1);
-                int difference = sequence.get(sequence.size() - 1) + upperSequence.get(upperSequence.size() - 1);
-                upperSequence.add(difference);
-            }
-            return history;
+        private static void extrapolate(List<Integer> sequence, List<Integer> upperSequence) {
+            int difference = sequence.get(sequence.size() - 1) + upperSequence.get(upperSequence.size() - 1);
+            upperSequence.add(difference);
         }
+    }
+
+    public static class Solution2 {
+        public static void main(String[] args) {
+            try (BufferedReader reader = ResourceUtils.resourceReader(MirageMaintenance.class, REPORT)) {
+                int result = reader.lines()
+                        .map(MirageMaintenance::lineToValueHistory)
+                        .map(history -> extrapolateValueHistory(history, Solution2::extrapolate))
+                        .mapToInt(history -> history.get(0))
+                        .sum();
+                logger.log(Level.INFO, "Result = {0}", result);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "Unable to read input file", ex);
+            }
+        }
+
+        private static void extrapolate(List<Integer> sequence, List<Integer> upperSequence) {
+            int difference = upperSequence.get(0) - sequence.get(0) ;
+            upperSequence.add(0, difference);
+        }
+    }
+
+    private static List<Integer> extrapolateValueHistory(List<Integer> history,
+                                                         BiConsumer<List<Integer>, List<Integer>> extrapolation) {
+        List<List<Integer>> differences = prepareDifferences(history);
+
+        for (int i = differences.size() - 1; i > 0; i--) {
+            List<Integer> sequence = differences.get(i);
+            List<Integer> upperSequence = differences.get(i - 1);
+            extrapolation.accept(sequence, upperSequence);
+        }
+        return history;
+    }
+
+    private static List<List<Integer>> prepareDifferences(List<Integer> history) {
+        List<List<Integer>> differences = new ArrayList<>();
+        differences.add(history);
+
+        List<Integer> currentSequence = history;
+
+        while (!currentSequence.stream().allMatch(value -> value == 0)) {
+            List<Integer> nextSequence = new ArrayList<>();
+            for (int i = 0; i < currentSequence.size() - 1; i++) {
+                int difference =  currentSequence.get(i + 1) - currentSequence.get(i);
+                nextSequence.add(difference);
+            }
+            if (!nextSequence.isEmpty()) {
+                differences.add(nextSequence);
+            }
+            currentSequence = nextSequence;
+        }
+        return differences;
     }
 
     private static List<Integer> lineToValueHistory(String line) {
